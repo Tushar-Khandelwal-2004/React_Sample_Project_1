fbc77271b282a4074b974bb957f7cc3e3e95f2a2
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2024-12-29 17:41:45 +0530

    Added Figma Design Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// Figma Link -> https://www.figma.com/design/rephrU2FVgN8MFz6XhnP51/Learn-React-with-10-Projects?node-id=0-1&p=f&t=edklkUDVK0GmMGdG-0
+
 import "./App.css";
 import Navigation from "./components/Navigation";
 import HeroSection from "./components/Hero";
